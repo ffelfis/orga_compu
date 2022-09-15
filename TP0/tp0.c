1f8fffd40efc23a4db38157d9a694ee8610698f5
ffelfis <ffelfis@gmail.com>
2022-09-15 11:29:09 -0300

    Mensaje de ayuda.

--- TP0/tp0.c
+++ TP0/tp0.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 #define error_cant_param "Cantidad incorrecta de argumentos"
-#define mensaje_de_ayuda "Usage:\n \ttp0 -h"
+#define mensaje_de_ayuda "Usage:\n \ttp0 -h\n\ttp0 -V\n\ttp0 [options] file\nOptions:\n\t-V, --version\tPrint version and quit.\n\t-h, --help\tPrint this information.\n\t-l, --lines\tPrint number of lines in file.\n\t-w, --words\tPrint number of words in files.\n\t-c, --characters\tPrint number of characters in file.\n\t-i, --input\tPath to input file.\nExamples:\n\n\ttp0 -w -i input.txt"
 
 // Funcion para mostrar ayuda
 void ayuda(){
