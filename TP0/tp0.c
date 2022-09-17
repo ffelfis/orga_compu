742eb3abb854c3d7e72b8d4184cc4445255fe799
ffelfis <ffelfis@gmail.com>
2022-09-17 15:15:03 -0300

    Modificación mínima.

--- TP0/tp0.c
+++ TP0/tp0.c
@@ -5,23 +5,23 @@
 #define error_cant_param "Cantidad incorrecta de argumentos"
 #define mensaje_de_ayuda "Usage:\n \ttp0 -h\n\ttp0 -V\n\ttp0 [options] file\nOptions:\n\t-V, --version\t\tPrint version and quit.\n\t-h, --help\t\tPrint this information.\n\t-l, --lines\t\tPrint number of lines in file.\n\t-w, --words\t\tPrint number of words in files.\n\t-c, --characters\tPrint number of characters in file.\n\t-i, --input\t\tPath to input file.\nExamples:\n\n\ttp0 -w -i input.txt"
 
-// Función para mostrar ayuda
+// Muestra ayuda
 void ayuda(){
     printf("%s\n",mensaje_de_ayuda);
 }
 
-// Función para mostrar error y salir
-// fprintf se usa para escribir en stderr
+// Muestra error y termina ejecución
 void error(char *mensaje){
     fprintf(stderr,"%s\n",mensaje);
+    // fprintf se usa para escribir en stderr
     exit(1);
 }
 
-//argc -> argument count, argv -> argument vector
 int main(int argc, char *argv[]){
+//argc -> argument count, argv -> argument vector
 
     // Verifica la cantidad de argumentos
-    if (argc < 2 || argc > 5){
+    if (argc < 2 || argc > 4){
         error(error_cant_param);
     }
     // Verifica por pedido de ayuda en el primer argumento
