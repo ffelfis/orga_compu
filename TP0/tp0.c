edeb1a1ea06649baf3bcf80d716cfdab15e36477
ffelfis <ffelfis@gmail.com>
2022-09-18 02:27:45 -0300

    Agrega versión, mensajes y verificaciones de argumentos.

--- TP0/tp0.c
+++ TP0/tp0.c
@@ -3,31 +3,60 @@
 #include <string.h>
 
 #define error_cant_param "Cantidad incorrecta de argumentos"
+#define error_param_incorrectos "Las opciones ingresadas son incorrectas"
 #define mensaje_de_ayuda "Usage:\n \ttp0 -h\n\ttp0 -V\n\ttp0 [options] file\nOptions:\n\t-V, --version\t\tPrint version and quit.\n\t-h, --help\t\tPrint this information.\n\t-l, --lines\t\tPrint number of lines in file.\n\t-w, --words\t\tPrint number of words in files.\n\t-c, --characters\tPrint number of characters in file.\n\t-i, --input\t\tPath to input file.\nExamples:\n\n\ttp0 -w -i input.txt"
+#define version_num "tp0 v1.0"
 
 // Muestra ayuda
 void ayuda(){
     printf("%s\n",mensaje_de_ayuda);
 }
 
+// Muestra la versión del programa
+void version(){
+    printf("%s\n",version_num);
+}
+
 // Muestra error y termina ejecución
 void error(char *mensaje){
     fprintf(stderr,"%s\n",mensaje);
     // fprintf se usa para escribir en stderr
-    exit(1);
+    exit(0);
+    // exit termina la ejecución del programa
 }
 
 int main(int argc, char *argv[]){
 //argc -> argument count, argv -> argument vector
 
+    // Puntero a archivo
+    FILE *file;
+    char ch;
+
     // Verifica la cantidad de argumentos
     if (argc < 2 || argc > 4){
         error(error_cant_param);
     }
+
     // Verifica por pedido de ayuda en el primer argumento
     else if ((strcmp(argv[1], "-h") == 0) || (strcmp(argv[1], "--help") == 0)){
+        // Por si hay algo luego del pedido de ayuda
+        if (argc != 2) error(error_cant_param);
         ayuda();
         return 0;
     }
+
+    // Verifica por pedido de versión en el primer argumento
+    else if ((strcmp(argv[1], "-V") == 0) || (strcmp(argv[1], "--version") == 0)){
+        // Por si hay algo luego del pedido de versión
+        if (argc != 2) error(error_cant_param);
+        version();
+        return 0;
+    }
+
+    // Opciones ingresadas incorrectas
+    else {
+        error(error_param_incorrectos);
+    }
+
     return 0;
 }
