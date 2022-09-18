7a2d0c5144dcd3f87d04ad86a18b7ae71fd7026b
ffelfis <ffelfis@gmail.com>
2022-09-18 03:29:06 -0300

    Agrega opciones para palabras y líneas. No cuenta.

--- TP0/tp0.c
+++ TP0/tp0.c
@@ -31,6 +31,16 @@ int contar_caracteres(FILE *archivo){
     printf("Conteo de caracteres.\n");
 }
 
+// Cuenta palabras
+int contar_palabras(FILE *archivo){
+    printf("Conteo de palabras.\n");
+}
+
+// Cuenta líneas
+int contar_lineas(FILE *archivo){
+    printf("Conteo de líneas.\n");
+}
+
 int main(int argc, char *argv[]){
 //argc -> argument count, argv -> argument vector
 
@@ -58,8 +68,8 @@ int main(int argc, char *argv[]){
         return 0;
     }
 
-    // // Verifica por pedido de caracteres en el primer argumento y entrada en el segundo argumento
-    else if ((strcmp(argv[1], "-c") == 0) || (strcmp(argv[1], "--characters") == 0) && ((strcmp(argv[2], "-i")) || (strcmp(argv[2], "--input")))){
+    // Verifica por pedido de caracteres en el primer argumento y entrada en el segundo argumento
+    else if (((strcmp(argv[1], "-c") == 0) || (strcmp(argv[1], "--characters") == 0)) && ((strcmp(argv[2], "-i")) || (strcmp(argv[2], "--input")))){
         // Por si hay algo luego del nombre del archivo
         if (argc != 4) error(error_cant_param);
 
@@ -70,7 +80,35 @@ int main(int argc, char *argv[]){
         int num;
         num = contar_caracteres(file);
         return 0;
-    }    
+    }
+
+    // Verifica por pedido de palabras en el primer argumento y entrada en el segundo argumento
+    else if (((strcmp(argv[1], "-w") == 0) || (strcmp(argv[1], "--words") == 0)) && ((strcmp(argv[2], "-i")) || (strcmp(argv[2], "--input")))){
+        // Por si hay algo luego del nombre del archivo
+        if (argc != 4) error(error_cant_param);
+
+        // Se intenta abrir el archivo en modo lectura
+        file = fopen(argv[3], "r");
+        if (file == NULL) error(error_archivo_inexistente);
+
+        int num;
+        num = contar_palabras(file);
+        return 0;
+    }
+
+    // Verifica por pedido de líneas en el primer argumento y entrada en el segundo argumento
+    else if (((strcmp(argv[1], "-l") == 0) || (strcmp(argv[1], "--lines") == 0)) && ((strcmp(argv[2], "-i")) || (strcmp(argv[2], "--input")))){
+        // Por si hay algo luego del nombre del archivo
+        if (argc != 4) error(error_cant_param);
+
+        // Se intenta abrir el archivo en modo lectura
+        file = fopen(argv[3], "r");
+        if (file == NULL) error(error_archivo_inexistente);
+
+        int num;
+        num = contar_lineas(file);
+        return 0;
+    }     
 
     // Opciones ingresadas incorrectas
     else {
