1fe1a27bfb45b63db66c1b9315702b58a58226f5
ffelfis <ffelfis@gmail.com>
2022-10-11 20:56:31 -0300

    Verificación inicial y mensaje de error.

--- TP1/autcel.c
+++ TP1/autcel.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-int main()
+#define error_cant_param "Canidad incorrecta de argumentos.\nIngrese \"autcel -h\" o \"autcel --help\" para más información."
+
+/* Muestra el error y sale de la ejecución */
+void error(char *mensaje)
 {
-	printf("Hello, World!\n");
+	fprintf(stderr, "%s\n", mensaje);
+}
+
+int main(int argc, char *argv[])
+{
+	/* Verificación inicial de argumentos */
+	if (argc < 2 || argc > 6)
+	{
+		error(error_cant_param);
+	}
+
+	printf("Fin de ejecución.\n");
+
 	return 0;
 }
