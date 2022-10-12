79a02eb68bdb19f92995d7a34438210dc492ac63
ffelfis <ffelfis@gmail.com>
2022-10-12 20:01:47 -0300

    pruebas_tp1.sh: ayuda y versión.

---
+++ TP1/pruebas_tp1.sh
@@ -0,0 +1,16 @@
+# printf "username@hostname:~/directories$ ./tp1 -h\n"
+printf "$ ./tp1 -h\n"
+./tp1 -h
+printf "\n"
+
+printf "$ ./tp1 --help\n"
+./tp1 --help
+printf "\n"
+
+printf "$ ./tp1 -V\n"
+./tp1 -V
+printf "\n"
+
+printf "$ ./tp1 --version\n"
+./tp1 --version
+printf "\n"
