d7fede8275651c1309cf88837180e7c6fdbb1528
ffelfis <ffelfis@gmail.com>
2022-10-11 22:36:36 -0300

    Versión. Corrección de mensaje de ayuda.

--- TP1/autcel.c
+++ TP1/autcel.c
@@ -4,7 +4,8 @@
 
 #define error_cant_param "Cantidad incorrecta de argumentos.\nIngrese \"autcel -h\" o \"autcel --help\" para más información."
 #define error_param_incorretos "Los argumentos ingresados son incorrectos.\nIngrese \"autcel -h\" o \"autcel --help\" para más información."
-#define mensaje_ayuda "Uso:\n\tautcel -h\n\tautcel -V\n\tautcel R N inputfile [-o outputprefix]\nOpciones:\n\t-h, --help\t\tImprime este mensaje.\n\t-V, --version\t\tDa la version del programa.\n\t-o\t\tPrefijo de los archivos de salida.\nEjemplos:\n\tautcel 30 80 inicial -o evolucion\n\tCalcula la evolución del autómata \"Regla 30\",\n\ten un mundo unidimensional de 80 celdas, por 80 iteraciones.\n\tEl archivo de salida se llamará evolucion.pbm.\n\tSi no se da un prefijo para los archivos de salida,\n\tel prefijo será el nombre del archivo de entrada."
+#define mensaje_ayuda "Uso:\n\tautcel -h\n\tautcel -V\n\tautcel R N inputfile [-o outputprefix]\nOpciones:\n\t-h, --help\tImprime este mensaje.\n\t-V, --version\tDa la version del programa.\n\t-o\t\tPrefijo de los archivos de salida.\nEjemplos:\n\tautcel 30 80 inicial -o evolucion\n\tCalcula la evolución del autómata \"Regla 30\",\n\ten un mundo unidimensional de 80 celdas, por 80 iteraciones.\n\tEl archivo de salida se llamará evolucion.pbm.\n\tSi no se da un prefijo para los archivos de salida,\n\tel prefijo será el nombre del archivo de entrada."
+#define mensaje_version "autcel v1.0"
 
 /* Muestra el error y sale de la ejecución */
 void error(char *mensaje)
@@ -17,6 +18,12 @@ void error(char *mensaje)
 void ayuda()
 {
 	printf("%s\n", mensaje_ayuda);
+}
+
+/* Versión */
+void version()
+{
+	printf("%s\n", mensaje_version);
 }
 
 int main(int argc, char *argv[])
@@ -35,13 +42,23 @@ int main(int argc, char *argv[])
         ayuda();
         return 0;
 	}
+
+	/* Verifica pedido de versión */
+	if (strcmp(argv[1], "-V") == 0 || strcmp(argv[1], "--version") == 0)
+	{
+		/* Verifica que la cantidad de argumentos sea 2 */
+        if (argc != 2) error(error_cant_param);
+        version(mensaje_version);
+        return 0;
+	}
+
 	/* Captura error de argumentos */
 	else
 	{
 		error(error_param_incorretos);
 	}
 
-	printf("Fin de ejecución.\n");
+	printf("Pasa lectura de argumentos.\n");
 
 	return 0;
 }
