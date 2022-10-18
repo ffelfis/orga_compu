b0e89a12afeb2cc75f4dd4ab3090d89071608105
ffelfis <ffelfis@gmail.com>
2022-10-18 18:55:56 -0300

    Desarrollo tp1_facu.

---
+++ TP1/misc/tp1_facu.c
@@ -0,0 +1,162 @@
+#include <stdio.h> /*FILE, fprint(), fopen(), fclose(), rewind()*/
+#include <string.h> /*strcmp()*/
+
+#include "utilidades.h"
+
+#define error_cant_param "Cantidad incorrecta de argumentos.\nIngrese \"autcel -h\" o \"autcel --help\" para más información."
+#define error_param_incorretos "Los argumentos ingresados son incorrectos.\nIngrese \"autcel -h\" o \"autcel --help\" para más información."
+#define error_inval_regla "La regla ingresada en inválida"
+#define error_inval_celda "El número de celdas es inválido"
+#define error_archivo_inexistente "El archivo que intenta abrir no existe"
+#define error_prefijo_salida "Prefijo de salida inválido"
+//#define error_formato_archivo "El formato del archivo o número de celdas es incorrecto"
+#define mensaje_ayuda "Uso:\n\tautcel -h\n\tautcel -V\n\tautcel R N inputfile [-o outputprefix]\nOpciones:\n\t-h, --help\tImprime este mensaje.\n\t-V, --version\tDa la version del programa.\n\t-o\t\tPrefijo de los archivos de salida.\nEjemplos:\n\tautcel 30 80 inicial -o evolucion\n\tCalcula la evolución del autómata \"Regla 30\",\n\ten un mundo unidimensional de 80 celdas, por 80 iteraciones.\n\tEl archivo de salida se llamará evolucion.pbm.\n\tSi no se da un prefijo para los archivos de salida,\n\tel prefijo será el nombre del archivo de entrada."
+#define mensaje_version "autcel v1.0"
+
+int main(int argc, char *argv[])
+{
+	unsigned int numRegla; /* Guardará el número de regla */
+	unsigned int numCelda; /* Guardará el número de celdas */
+
+	FILE *file; /* Puntero al archivo de entrada */
+
+	/* Verificación inicial de argumentos */
+	if (argc < 2 || argc > 6)
+	{
+		error(error_cant_param);
+	}
+
+	/* Verifica pedido de ayuda en argumento 1 */
+	if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
+	{
+		/* Verifica que la cantidad de argumentos sea 2 */
+        if (argc != 2) error(error_cant_param);
+        printf("%s\n", mensaje_ayuda);
+        return 0;
+	}
+
+	/* Verifica pedido de versión en argumento 1 */
+	if (strcmp(argv[1], "-V") == 0 || strcmp(argv[1], "--version") == 0)
+	{
+		/* Verifica que la cantidad de argumentos sea 2 */
+        if (argc != 2) error(error_cant_param);
+        printf("%s\n", mensaje_version);
+        return 0;
+	}
+
+	/* Verifica regla, tamaño de la matriz y archivo para 4 argumentos */
+	if (argc == 4)
+	{
+		printf("Tiene 4 argmentos.\n");
+
+		numRegla = validateNum(argv[1], error_inval_regla);
+
+		numCelda = validateNum(argv[2], error_inval_celda);
+
+		/* Verifica si el archivo de argv[3] existe */
+		printf("Archivo semilla: %s\n", argv[3]);
+		file = fopen(argv[3], "r");
+		if (file == NULL) error(error_archivo_inexistente);
+		validarSeed(file, numCelda);
+
+		/* Crear matrix de dimensión numCelda x numCelda */
+
+		/* Completar primera fila de matriz */
+
+		/* Usar proximo() */
+
+		/* Crear archivo de salida */
+
+		printf("Archivo salida: %s.pbm\n", argv[3]);
+
+		fclose(file);
+
+		return 0; /* Termina la ejecución */
+	}
+
+	/* Verifica regla y tamaño de la matriz para 6 argumentos */
+	if (argc == 6)
+	{
+		printf("Tiene 6 argmentos.\n");
+
+		numRegla = validateNum(argv[1], error_inval_regla);
+
+		numCelda = validateNum(argv[2], error_inval_celda);
+
+		/* Verifica si el archivo de argv[3] existe */
+		printf("Archivo semilla: %s\n", argv[3]);
+		file = fopen(argv[3], "r");
+		if (file == NULL) error(error_archivo_inexistente);
+		validarSeed(file, numCelda);
+		rewind(file); /* Resetea el puntero al inicio del archivo */
+
+		if (strcmp(argv[4], "-o") == 0)
+		{
+			printf("Tiene prefijo para salida: %s\n", argv[4]);
+		}
+		else
+		{
+			error(error_prefijo_salida);
+		}
+
+		/* Crear matrix de dimensión numCelda x numCelda */
+		printf("Crea la matriz\n");
+		unsigned char matriz[numCelda][numCelda];
+
+		/* Completar primera fila de matriz */
+		printf("Completa la fila 1\n");
+		
+		/*  matriz: puntero a primer elemento de arreglo de 2 dimensiones */
+		/* *matriz: puntero a primer elemento de arreglo[numCelda] -> caracter */
+		unsigned char *ptr_matriz = *matriz;
+		
+		fillRow1(file, ptr_matriz, numCelda);
+
+		/* IMPRIME PRIMERA FILA DE MATRIZ BORRAR */
+
+		for (int i=0; i<numCelda; i++)
+		{
+			printf("%c ", matriz[0][i]);
+		}
+		printf("\n");
+
+		/* Usar proximo() */
+		ptr_matriz = *matriz; /* Reset posición (0, 0)*/
+		/* numCelda-1 ya que la primer fila está completa */
+		for (int i=0; i<(numCelda-1); i++)
+		{
+			for (int j=0; j<numCelda; j++)
+			{
+				matriz[i+1][j] = proximo(ptr_matriz, i, j, numRegla, numCelda);
+			}
+		}
+
+		/* IMPRIMIR MATRIZ */
+		for (int i=0; i<numCelda; i++)
+		{
+			for (int j=0; j<numCelda; j++)
+			{
+				printf("%c ", matriz[i][j]);
+			}
+			printf("\n");
+		}
+
+		/* Crear archivo de salida */
+
+		printf("Archivo salida: %s.pbm\n", argv[5]);
+
+		fclose(file);
+
+		return 0; /* Termina la ejecución */
+	}
+
+	/* Captura error de argumentos: último else */
+	else
+	{
+		error(error_param_incorretos);
+	}
+
+	printf("Pasa lectura de argumentos.\n"); /*BORRAR*/
+
+	return 0;
+}
---
+++ TP1/misc/utilidades.h
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h> /*exit(), strtol()*/
+#include <stdbool.h> /*true, false*/
+#include <ctype.h> /*isdigit(), isspace()*/
+
+#define error_caracter "El archivo tiene caracteres inválidos"
+#define error_numCelda "La cantidad de celdas y caracteres no coincide"
+
+
+/// @brief Muestra mensaje de error y sale de la ejecución
+/// @param mensaje 
+void error(char *mensaje)
+{
+	fprintf(stderr, "%s\n", mensaje);
+	exit(1);
+}
+
+
+/// @brief Verifica números en cadena (palabra)
+/// @param palabra 
+/// @param mensaje_error De no ser número termina la ejecución con mensaje de error
+/// @return número válido como unsigned int
+unsigned int validateNum(char *palabra, char* mensaje_error)
+{
+	unsigned int numero;
+	char *resto;
+	
+	numero = (int) strtol(palabra, &resto, 10);
+
+	/* Verifica si el string es solo un número positivo y nada más */
+	if (!strcmp(resto, "") == 0 || numero == 0 || numero < 0)
+	{
+		error(mensaje_error);
+	}
+	else printf("Función isValid: El número es válido.\n");
+	return numero;
+}
+
+
+/// @brief Verifica {'0', '1'}, {#celdas == #caracteres}, si no termina y devuelve error
+/// @param seedFile 
+/// @param numCelda 
+void validarSeed(FILE *seedFile, unsigned int numCelda)
+{
+	char ch; /* Lectura de caracter */
+	char last; /* Guarda el último caracter (REDUNDANTE) */
+	unsigned int contador = 0; /* Cuenta caracteres */
+
+	while ((ch = fgetc(seedFile)) != EOF)
+	{
+		last = ch; /* (REDUNDANTE) */
+		/* Si no es white space: '\n' ' ' EOL */
+		if (!isspace(ch))
+        {
+			/* Si no es '0' ó '1': error */
+			if (!(ch == '0' || ch == '1'))
+			{
+				error(error_caracter);
+			}
+			contador++;
+        }
+		/* Si es white space distinto a EOL: error */
+        else if (isspace(ch) && (ch != 10))
+        {
+			error(error_caracter);
+        }
+	}
+
+	/* verifica que el último caracter sea el EOL (REDUNDANTE) */
+	if (last != 10)
+	{
+		error(error_caracter);
+	}
+
+	/* Verifica que la cantidad de caracteres coincida con numCelda */
+	if (contador != numCelda)
+	{
+		error(error_numCelda);
+	}
+	/* Resetea el puntero al inicio del archivo */ 
+	//rewind(seedFile);
+}
+
+
+/// @brief Rellena la primera fila con los valores del archivo de entrada
+/// @param seedFile 
+/// @param a puntero a la posición (0, 0) de la matriz
+/// @param N Cantidad de filas y columnas de la matriz
+void fillRow1(FILE *seedFile, unsigned char *a, unsigned int N)
+{
+	unsigned char ch;
+
+	for (int i=0; i<N; i++)
+	{
+		*a = fgetc(seedFile);
+		a++;
+	}
+}
+
+
+/// @brief Calcula el próximo estado de la celda indicada por (i, j)
+/// @param a puntero a posición (0, 0) de la matriz
+/// @param i fila de celda actual
+/// @param j columna de celda actual
+/// @param regla 
+/// @param N Cantidad de filas y columnas de la matriz
+/// @return Próximo estado de la celda
+unsigned char proximo(unsigned char *a, unsigned int i, unsigned int j, unsigned char regla, unsigned int N)
+{
+	int divisiones = 0; 
+	unsigned char valor = regla;
+	int salto = N-1;
+
+	printf("Calculo de proximo: (%d, %d) ", i+1, j);
+
+	printf("cuenta: %d ", (*(a+2)-'0') * 4);
+
+	if (j == 0)
+	{
+		divisiones = ((*(a+salto) - '0') * 4) + ((*a - '0') * 2) + ((*(a+1) - '0') * 1);
+		printf("ini ciclos: %d \n", divisiones);
+	}
+	if (j == N-1)
+	{
+		divisiones = ((*(a-1) - '0') * 4) + ((*a - '0') * 2) + ((*(a-salto) - '0') * 1);
+		printf("med ciclos: %d \n", divisiones);
+	}
+	else
+	{
+		divisiones = ((*(a-1) - '0') * 4) + ((*a - '0') * 2) + ((*(a+1) - '0') * 1);
+		printf("fin ciclos: %d \n", divisiones);
+	}
+
+	for (int k=0; k<divisiones; k++)
+	{
+		valor = valor / 2;
+	}
+
+	return (valor % 2);
+}
